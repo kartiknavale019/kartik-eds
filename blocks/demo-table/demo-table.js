428dd7cb286f311a9d08b1ede397c1f95c230e42
kartik navale <kartiknavale@deptagency.com>
2026-04-01 12:32:41 +0530

    duplicate table bug fix

--- blocks/demo-table/demo-table.js
+++ blocks/demo-table/demo-table.js
@@ -1,5 +1,2 @@
 export default function decorate(block) {
-  // We leave this empty! 
-  // By not destroying the default <div> elements AEM creates, 
-  // the Universal Editor can successfully track and display our rows.
 }
